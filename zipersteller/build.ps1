ed03a15a1ce8b453891c480f61a655c850a9aa92
Treeguy-AN <bau.phil@web.de>
2026-05-25 11:54:54 +0200

    Ordner Struktur berichtigt

--- zipersteller/build.ps1
+++ zipersteller/build.ps1
@@ -1,38 +1,56 @@
 $sfml = "C:\Users\tbaum\OneDrive\Desktop\SFML-2.6.2"
 
-$gameFolder = "..\Game"
-$output = "release"
-$zipName = "realm-fighters.zip"
-$downloadFolder = "..\downloads"
+$root = ".."
 
-if (Test-Path $output) {
-    Remove-Item $output -Recurse -Force
+$gameFolder = "$root\Game"
+$releaseFolder = "$gameFolder\release"
+
+$downloadFolder = "$root\downloads"
+
+$zipName = "realm-fighters.zip"
+
+if (Test-Path $releaseFolder) {
+    Remove-Item $releaseFolder -Recurse -Force
 }
 
-New-Item -ItemType Directory -Path $output | Out-Null
+New-Item -ItemType Directory -Path $releaseFolder | Out-Null
 
 g++ `
 "$gameFolder\main.cpp" `
 "$gameFolder\game.cpp" `
 "$gameFolder\fighter.cpp" `
--o "$output\realm-fighters.exe" `
+-o "$releaseFolder\realm-fighters.exe" `
 -I"$sfml\include" `
 -L"$sfml\lib" `
 -lsfml-graphics `
 -lsfml-window `
 -lsfml-system
 
-Copy-Item "$sfml\bin\sfml-graphics-2.dll" $output
-Copy-Item "$sfml\bin\sfml-window-2.dll" $output
-Copy-Item "$sfml\bin\sfml-system-2.dll" $output
+Copy-Item "$sfml\bin\sfml-graphics-2.dll" $releaseFolder
+Copy-Item "$sfml\bin\sfml-window-2.dll" $releaseFolder
+Copy-Item "$sfml\bin\sfml-system-2.dll" $releaseFolder
 
-Copy-Item "$gameFolder\arial.ttf" $output -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Path "$releaseFolder\assets\fonts" -Force | Out-Null
+
+Copy-Item `
+"$gameFolder\assets\fonts\arial.ttf" `
+"$releaseFolder\assets\fonts\arial.ttf" `
+-Force
 
 if (!(Test-Path $downloadFolder)) {
     New-Item -ItemType Directory -Path $downloadFolder | Out-Null
 }
 
-Compress-Archive -Path "$output\*" -DestinationPath "$downloadFolder\$zipName" -Force
+Compress-Archive `
+-Path "$releaseFolder\*" `
+-DestinationPath "$downloadFolder\$zipName" `
+-Force
 
-Write-Host "ZIP erstellt:"
+Write-Host ""
+Write-Host "Build erfolgreich!"
+Write-Host ""
+Write-Host "Release Ordner:"
+Write-Host $releaseFolder
+Write-Host ""
+Write-Host "ZIP:"
 Write-Host "$downloadFolder\$zipName"
